e88bfb59f957a3e48b60fbd5f4d9000b47cd0365
Hao-kang Den <haokang.den@gmail.com>
2013-03-07 21:14:29 +0800

    expose to global if no markdownEl

--- src/strapdown.js
+++ src/strapdown.js
@@ -1,132 +1,137 @@
 ;(function(window, document) {
 
-  // Hide body until we're done fiddling with the DOM
-  document.body.style.display = 'none';
+  var markdownEl = document.getElementsByTagName('xmp')[0] || document.getElementsByTagName('textarea')[0];
 
-  //////////////////////////////////////////////////////////////////////
-  //
-  // Shims for IE < 9
-  //
+  // If markdownEl are inserted afterwards, expose strapdown to global namespace.
+  markdownEl ? strapdown(window, document) : window.strapdown = strapdown;
 
-  document.head = document.getElementsByTagName('head')[0];
+  function strapdown(window, document) {
+    // Hide body until we're done fiddling with the DOM
+    document.body.style.display = 'none';
 
-  if (!('getElementsByClassName' in document)) {
-    document.getElementsByClassName = function(name) {
-      function getElementsByClassName(node, classname) {
-        var a = [];
-        var re = new RegExp('(^| )'+classname+'( |$)');
-        var els = node.getElementsByTagName("*");
-        for(var i=0,j=els.length; i<j; i++)
-            if(re.test(els[i].className))a.push(els[i]);
-        return a;
+    //////////////////////////////////////////////////////////////////////
+    //
+    // Shims for IE < 9
+    //
+
+    document.head = document.getElementsByTagName('head')[0];
+
+    if (!('getElementsByClassName' in document)) {
+      document.getElementsByClassName = function(name) {
+        function getElementsByClassName(node, classname) {
+          var a = [];
+          var re = new RegExp('(^| )'+classname+'( |$)');
+          var els = node.getElementsByTagName("*");
+          for(var i=0,j=els.length; i<j; i++)
+              if(re.test(els[i].className))a.push(els[i]);
+          return a;
+        }
+        return getElementsByClassName(document.body, name);
       }
-      return getElementsByClassName(document.body, name);
     }
-  }
 
-  //////////////////////////////////////////////////////////////////////
-  //
-  // Get user elements we need
-  //
+    //////////////////////////////////////////////////////////////////////
+    //
+    // Get user elements we need
+    //
+    var markdownEl = document.getElementsByTagName('xmp')[0] || document.getElementsByTagName('textarea')[0],
+        titleEl = document.getElementsByTagName('title')[0],
+        scriptEls = document.getElementsByTagName('script'),
+        navbarEl = document.getElementsByClassName('navbar')[0];
 
-  var markdownEl = document.getElementsByTagName('xmp')[0] || document.getElementsByTagName('textarea')[0],
-      titleEl = document.getElementsByTagName('title')[0],
-      scriptEls = document.getElementsByTagName('script'),
-      navbarEl = document.getElementsByClassName('navbar')[0];
+    //////////////////////////////////////////////////////////////////////
+    //
+    // <head> stuff
+    //
 
-  //////////////////////////////////////////////////////////////////////
-  //
-  // <head> stuff
-  //
+    // Use <meta> viewport so that Bootstrap is actually responsive on mobile
+    var metaEl = document.createElement('meta');
+    metaEl.name = 'viewport';
+    metaEl.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0';
+    if (document.head.firstChild)
+      document.head.insertBefore(metaEl, document.head.firstChild);
+    else
+      document.head.appendChild(metaEl);
 
-  // Use <meta> viewport so that Bootstrap is actually responsive on mobile
-  var metaEl = document.createElement('meta');
-  metaEl.name = 'viewport';
-  metaEl.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0';
-  if (document.head.firstChild)
-    document.head.insertBefore(metaEl, document.head.firstChild);
-  else
-    document.head.appendChild(metaEl);
-
-  // Get origin of script
-  var origin = '';
-  for (var i = 0; i < scriptEls.length; i++) {
-    if (scriptEls[i].src.match('strapdown')) {
-      origin = scriptEls[i].src;
+    // Get origin of script
+    var origin = '';
+    for (var i = 0; i < scriptEls.length; i++) {
+      if (scriptEls[i].src.match('strapdown')) {
+        origin = scriptEls[i].src;
+      }
     }
+    var originBase = origin.substr(0, origin.lastIndexOf('/'));
+
+    // Get theme
+    var theme = markdownEl.getAttribute('theme') || 'bootstrap';
+    theme = theme.toLowerCase();
+
+    // Stylesheets
+    var linkEl = document.createElement('link');
+    linkEl.href = originBase + '/themes/'+theme+'.min.css';
+    linkEl.rel = 'stylesheet';
+    document.head.appendChild(linkEl);
+
+    var linkEl = document.createElement('link');
+    linkEl.href = originBase + '/strapdown.css';
+    linkEl.rel = 'stylesheet';
+    document.head.appendChild(linkEl);
+
+    var linkEl = document.createElement('link');
+    linkEl.href = originBase + '/themes/bootstrap-responsive.min.css';
+    linkEl.rel = 'stylesheet';
+    document.head.appendChild(linkEl);
+
+    //////////////////////////////////////////////////////////////////////
+    //
+    // <body> stuff
+    //
+
+    var markdown = markdownEl.textContent || markdownEl.innerText;
+
+    var newNode = document.createElement('div');
+    newNode.className = 'container';
+    newNode.id = 'content';
+    document.body.replaceChild(newNode, markdownEl);
+
+    // Insert navbar if there's none
+    var newNode = document.createElement('div');
+    newNode.className = 'navbar navbar-fixed-top';
+    if (!navbarEl && titleEl) {
+      newNode.innerHTML = '<div class="navbar-inner"> <div class="container"> <div id="headline" class="brand"> </div> </div> </div>';
+      document.body.insertBefore(newNode, document.body.firstChild);
+      var title = titleEl.innerHTML;
+      var headlineEl = document.getElementById('headline');
+      if (headlineEl)
+        headlineEl.innerHTML = title;
+    }
+
+    //////////////////////////////////////////////////////////////////////
+    //
+    // Markdown!
+    //
+
+    // Generate Markdown
+    var html = marked(markdown);
+    document.getElementById('content').innerHTML = html;
+
+    // Prettify
+    var codeEls = document.getElementsByTagName('code');
+    for (var i=0, ii=codeEls.length; i<ii; i++) {
+      var codeEl = codeEls[i];
+      var lang = codeEl.className;
+      codeEl.className = 'prettyprint lang-' + lang;
+    }
+    prettyPrint();
+
+    // Style tables
+    var tableEls = document.getElementsByTagName('table');
+    for (var i=0, ii=tableEls.length; i<ii; i++) {
+      var tableEl = tableEls[i];
+      tableEl.className = 'table table-striped table-bordered';
+    }
+
+    // All done - show body
+    document.body.style.display = '';
   }
-  var originBase = origin.substr(0, origin.lastIndexOf('/'));
-
-  // Get theme
-  var theme = markdownEl.getAttribute('theme') || 'bootstrap';
-  theme = theme.toLowerCase();
-
-  // Stylesheets
-  var linkEl = document.createElement('link');
-  linkEl.href = originBase + '/themes/'+theme+'.min.css';
-  linkEl.rel = 'stylesheet';
-  document.head.appendChild(linkEl);
-
-  var linkEl = document.createElement('link');
-  linkEl.href = originBase + '/strapdown.css';
-  linkEl.rel = 'stylesheet';
-  document.head.appendChild(linkEl);
-
-  var linkEl = document.createElement('link');
-  linkEl.href = originBase + '/themes/bootstrap-responsive.min.css';
-  linkEl.rel = 'stylesheet';
-  document.head.appendChild(linkEl);
-
-  //////////////////////////////////////////////////////////////////////
-  //
-  // <body> stuff
-  //
-
-  var markdown = markdownEl.textContent || markdownEl.innerText;
-
-  var newNode = document.createElement('div');
-  newNode.className = 'container';
-  newNode.id = 'content';
-  document.body.replaceChild(newNode, markdownEl);
-
-  // Insert navbar if there's none
-  var newNode = document.createElement('div');
-  newNode.className = 'navbar navbar-fixed-top';
-  if (!navbarEl && titleEl) {
-    newNode.innerHTML = '<div class="navbar-inner"> <div class="container"> <div id="headline" class="brand"> </div> </div> </div>';
-    document.body.insertBefore(newNode, document.body.firstChild);
-    var title = titleEl.innerHTML;
-    var headlineEl = document.getElementById('headline');
-    if (headlineEl)
-      headlineEl.innerHTML = title;
-  }
-
-  //////////////////////////////////////////////////////////////////////
-  //
-  // Markdown!
-  //
-
-  // Generate Markdown
-  var html = marked(markdown);
-  document.getElementById('content').innerHTML = html;
-
-  // Prettify
-  var codeEls = document.getElementsByTagName('code');
-  for (var i=0, ii=codeEls.length; i<ii; i++) {
-    var codeEl = codeEls[i];
-    var lang = codeEl.className;
-    codeEl.className = 'prettyprint lang-' + lang;
-  }
-  prettyPrint();
-
-  // Style tables
-  var tableEls = document.getElementsByTagName('table');
-  for (var i=0, ii=tableEls.length; i<ii; i++) {
-    var tableEl = tableEls[i];
-    tableEl.className = 'table table-striped table-bordered';
-  }
-
-  // All done - show body
-  document.body.style.display = '';
-
 })(window, document);
